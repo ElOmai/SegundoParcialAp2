d60d56e092ef7147ab4cc0acc699f507b499872f
Luis Omar Emilio Perez Rodriguez <luis_perez6@ucne.edu.do>
2020-03-11 15:51:40 -0400

    Add project files.

---
+++ SegundoParcialAp2/Data/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SegundoParcialAp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcialAp2.Data
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Prueba> Pruebas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= Database/Data.db");
+        }
+    }
+}
